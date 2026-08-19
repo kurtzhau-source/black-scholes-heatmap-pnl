6fb357ead9cf2e2589aee7b39df5ebf8881bb3dd
kurtzhau-source <kurtzhau@gmail.com>
2026-08-19 15:34:25 +0100

    more adjustments

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.title("Profit & Loss heatmap for the Black-Scholes pricing equation")
 
 st.write("---")
 
-col_dimension, col_annot = st.columns(2)
+col_dimension, col_annot, empty1, empty2 = st.columns(4)
 with col_dimension:
   with st.container():
             num = int(st.number_input("Dimension of PnL heatmap", min_value=2, value=6, step=1))
